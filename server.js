9b7abe125f3c0ebda469e1f5de209520875a88f8
Lahiru Sandaruwan <lahirus250@gmail.com>
2024-09-19 19:14:50 +0530

    created simple server with root route

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-// const express = require('express');
-// const app = express();
-// const port = 9000
+const express = require("express");
+const app = express();
+const port = 9000;
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
